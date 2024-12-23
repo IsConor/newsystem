d992314469e973a84764d833befddb6696e402ed
IsConor <baopengb0@outlook.com>
2024-12-23 15:49:36 +0800

    动态渲染菜单

--- src/components/sandbox/SideMenu.js
+++ src/components/sandbox/SideMenu.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Layout, Menu, Switch } from "antd";
+import '../../App.css'
 import { withRouter } from "react-router";
 import {
   LockOutlined,
@@ -9,10 +10,42 @@ import {
   FolderOpenOutlined,
   ApartmentOutlined,
   SolutionOutlined,
+  BarsOutlined,
+  FolderAddOutlined,
+  FolderViewOutlined,
+  SignatureOutlined,
+  SisternodeOutlined,
+  ShareAltOutlined,
+  WifiOutlined,
+  SendOutlined,
+  LoadingOutlined,
+  EyeInvisibleOutlined,
 } from "@ant-design/icons";
 import "./index.css";
+import axios from "axios";
 
 const { Sider } = Layout;
+
+// 菜单图标
+const iconList = {
+  "/home": <HomeOutlined />,
+  "/user-manage": <IdcardOutlined />,
+  "/user-manage/list": <SolutionOutlined />,
+  "/right-manage": <LockOutlined />,
+  "/right-manage/role/list": <UserOutlined />,
+  "/right-manage/right/list": <ApartmentOutlined />,
+  "/news-manage": <BarsOutlined />,
+  "/news-manage/add": <FolderAddOutlined />,
+  "/news-manage/ready": <FolderViewOutlined />,
+  "/news-manage/class": <FolderOpenOutlined />,
+  "/audit-manage": <ShareAltOutlined />,
+  "/audit-manage/news": <SignatureOutlined />,
+  "/audit-manage/list": <SisternodeOutlined />,
+  "/publish-manage": <WifiOutlined />,
+  "/publish-manage/ready": <LoadingOutlined />,
+  "/publish-manage/send": <SendOutlined />,
+  "/publish-manage/del": <EyeInvisibleOutlined />,
+};
 
 function SideMenu(props) {
   // 当前主题为深色主题
@@ -21,73 +54,84 @@ function SideMenu(props) {
   const changeTheme = (value) => {
     setTheme(value ? "dark" : "light");
   };
+  const [menuList, setMenuList] = useState([]);
 
-  // menu菜单结构
-  const menuList = [
-    {
-      key: "/home",
-      icon: <HomeOutlined />,
-      label: "主页",
-    },
-    {
-      key: "/user-manage",
-      icon: <IdcardOutlined />,
-      label: "员工管理",
-      children: [
-        {
-          key: "/user-manage/list",
-          label: "员工列表",
-          icon: <SolutionOutlined />,
-        },
-      ],
-    },
-    {
-      key: "3",
-      icon: <LockOutlined />,
-      label: "权限管理",
-      children: [
-        {
-          key: "/right-manage/role/list",
-          label: "角色列表",
-          icon: <UserOutlined />,
-        },
-        {
-          key: "/right-manage/right/list",
-          label: "权限列表",
-          icon: <ApartmentOutlined />,
-        },
-      ],
-    },
-    {
-      key: "4",
-      icon: <FolderOpenOutlined />,
-      label: "草稿箱",
-    },
-  ];
+  // 组件挂载成功hooks,获取menu菜单
+  useEffect(() => {
+    axios.get("http://localhost:8000/rights?_embed=children").then((res) => {
+      setMenuList(renderMenu(res.data));
+    });
+  }, []);
+
+  // 分类menu数据哪些展示到菜单
+  const checkPagePermission = (item) => {
+    return item.pagepermisson === 1;
+  };
+
+  // 处理menu数据，判断是否有子菜单
+  const renderMenu = (menuList) => {
+    return menuList.map((item) => {
+      let newMenu;
+      // 如果有childred并且children的内容不为空
+      if (item.children && item.children?.length > 0) {
+        newMenu = {
+          id: item.id,
+          key: item.key,
+          label: item.label,
+          pagepermisson: item.pagepermisson,
+          grade: item.grade,
+          icon: iconList[item.key],
+          children: renderMenu(item.children),
+        };
+      }
+      // 如果没有children
+      else {
+        // 筛选展示在左侧menu菜单中的数据
+        if (checkPagePermission(item)) {
+          newMenu = {
+            id: item.id,
+            key: item.key,
+            label: item.label,
+            // pagepermisson: item.pagepermisson,
+            grade: item.grade,
+            icon: iconList[item.key],
+          };
+        }
+        // 不展示到menu中的数据
+        else {
+        }
+      }
+      return newMenu;
+    });
+  };
 
   return (
     <Sider trigger={null} collapsible theme={theme}>
-      <div className="demo-logo-vertical">
-        <span className="title">PUWEI 云控 &nbsp;</span>
-        <Switch
-          checked={theme === "dark"}
-          onChange={changeTheme}
-          checkedChildren="Dark"
-          unCheckedChildren="Light"
-        />
+      <div style={{ display: "flex", height: "100%", flexDirection: "column" }}>
+        <div className="demo-logo-vertical">
+          <span className="title">PUWEI 云控 &nbsp;</span>
+          <Switch
+            checked={theme === "dark"}
+            onChange={changeTheme}
+            checkedChildren="Dark"
+            unCheckedChildren="Light"
+          />
+        </div>
+        <div style={{flex:1,overflow:"auto"}}>
+          <Menu
+            theme={theme}
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            items={menuList}
+            onClick={(item) => {
+              // 点击菜单跳转路由
+              props.history.push(item.key);
+            }}
+          />
+        </div>
       </div>
-      <Menu
-        theme={theme}
-        mode="inline"
-        defaultSelectedKeys={["1"]}
-        items={menuList}
-        onClick={(item)=>{
-          // 点击菜单跳转路由
-          props.history.push(item.key)
-        }}
-      />
     </Sider>
   );
 }
 // 通过withRouter将SideMenu组件包裹进Route组件
-export default withRouter(SideMenu)
+export default withRouter(SideMenu);
--- src/views/newssandbox/home/Home.js
+++ src/views/newssandbox/home/Home.js
@@ -1,11 +1,42 @@
-import React from 'react'
-import { Button } from 'antd'
+import React from "react";
+import { Button } from "antd";
+import axios from "axios";
 
 export default function Home() {
+  const ajax = () => {
+    // axios.get("http://localhost:8000/posts").then(res=>{
+    //   console.log(res.data)
+    // })
+
+    // axios.post("http://localhost:8000/posts", {
+    //   id: "3",
+    //   title: "new title",
+    //   views: 200,
+    // });
+
+    // 更新 patch局部更新（不影响其它项目） | 而put是全部更新
+    // axios.patch("http://localhost:8000/posts/1", {
+    //   title: "111-修改",
+    // });
+
+    // 删除 delete
+    // axios.delete("http://localhost:8000/posts/1")
+
+    // _embed 联合查询
+    // axios.get("http://localhost:8000/posts?_embed=comments").then(res=>{
+    //   console.log(res.data)
+    // })
+
+    // axios.get("http://localhost:8000/comments?_expand=post").then(res=>{
+    //   console.log(res.data)
+    // })
+
+  };
   return (
     <div>
-
-      <Button type='default' size='small'>Home</Button>
+      <Button type="default" size="small" onClick={ajax}>
+        Home
+      </Button>
     </div>
-  )
+  );
 }
